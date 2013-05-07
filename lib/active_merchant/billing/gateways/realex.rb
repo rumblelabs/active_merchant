e2e0de496fd98d9473781c566ef4112ebdae7e17
Iain Wilson <iain.iw.wilson@gmail.com>
2013-05-07 19:24:53 +0100

    Update Realex Gateway URL

--- lib/active_merchant/billing/gateways/realex.rb
+++ lib/active_merchant/billing/gateways/realex.rb
@@ -19,7 +19,7 @@ module ActiveMerchant
     # so if validation fails you can not correct and resend using the
     # same order id
     class RealexGateway < Gateway
-      self.live_url = self.test_url = 'https://epage.payandshop.com/epage-remote.cgi'
+      self.live_url = self.test_url = 'https://epage.payandshop.com/epage-remote-gw.cgi'
       RECURRING_PAYMENTS_URL = "https://epage.payandshop.com/epage-remote-plugins.cgi"
 
                   
